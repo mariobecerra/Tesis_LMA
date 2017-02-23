7389491112deaa33ff9378c148769be26b947edf
Dell <mbecerracontreras@gmail.com>
2017-02-22 19:16:57 -0600

    cambia gráfica a blanco y negro y reduce el número de valores de gamma, antes era 0 1 50 y ahora es 0 a 30

--- code/calcula_modelo_base.R
+++ code/calcula_modelo_base.R
@@ -1,9 +1,6 @@
-library(dplyr)
-library(tidyr)
 library(Matrix)
 library(Rcpp)
-library(ggplot2)
-library(gridExtra)
+library(tidyverse)
 
 if(!interactive()){ 
   args <- commandArgs(TRUE)
@@ -36,54 +33,6 @@ cat("Leyendo archivos de calificaciones\n")
 train_data <- readRDS(file_name_train)
 test_data <- readRDS(file_name_test)
 
-# archivo_calis <- paste0("../data/", dataset, "/ratings.csv")
-# 
-# #calis <- readr::read_csv('../data/ml-20m/ratings.csv') %>% 
-# cat("Leyendo archivo de calificaciones\n")
-# calis <- readr::read_csv(archivo_calis) %>%   
-#   select(userId,
-#          itemId_orig = itemId,
-#          rating) %>% 
-#   mutate(itemId = as.integer(factor(itemId_orig)))
-# head(calis)
-# 
-# cant_usuarios <- length(unique(calis$userId))
-# cant_pelis <- length(unique(calis$itemId))
-# 
-# ##############################################
-# ## Conjuntos de prueba y validación
-# ##############################################
-# 
-# cat("Creando conjuntos de entrenamiento y prueba\n")
-# set.seed(2805)
-# 
-# valida_usuarios <- sample(unique(calis$userId), cant_usuarios*.3 )
-# valida_items <- sample(unique(calis$itemId), cant_pelis*.3 )
-# 
-# dat_2 <- calis %>%
-#   mutate(valida_usu = userId %in% valida_usuarios) %>%
-#   mutate(valida_item = itemId %in% valida_items)
-# 
-# dat_train <- dat_2 %>% 
-#   filter(!valida_usu | !valida_item) %>% 
-#   select(-valida_usu, -valida_item)
-# 
-# dat_test <- dat_2 %>% 
-#   filter(valida_usu & valida_item) %>% 
-#   select(-valida_usu, -valida_item)
-# 
-# 
-# media_gral_train <- mean(dat_train$rating)
-# 
-# train_data <-dat_train %>% 
-#   mutate(u_id = as.integer(factor(userId)),
-#          rating_cent = rating - media_gral_train)
-# 
-# test_data <- dat_test %>% 
-#   left_join(unique(train_data[,c('userId', 'u_id')])) %>% 
-#   mutate(rating_cent = rating - media_gral_train) %>% 
-#   filter(!is.na(userId) & !is.na(itemId))
-
 ##############################################
 ## Modelo base
 ##############################################
@@ -99,22 +48,11 @@ promedios_items <- train_data %>%
   summarise(media_item = mean(rating),
             num_calis_item = n())
 
-# gamma <- 20
-# 
-# dat_test_3 <- test_data %>% 
-#   left_join(promedios_usuarios) %>% 
-#   left_join(promedios_items) %>% 
-#   mutate(rating_mb = media_gral_train + 
-#            (num_calis_usuario * (media_usuario - media_gral_train))/(gamma + num_calis_usuario) +
-#            (num_calis_item * (media_item - media_gral_train))/(gamma + num_calis_item)
-#          ) %>% 
-#   mutate(rating_mb = ifelse(is.na(rating_mb), media_gral_train, rating_mb))
-
 df_prueba <- test_data %>% 
   left_join(promedios_usuarios) %>% 
   left_join(promedios_items)
 
-errores_gamma <- sapply(0:50, function(gamma){
+errores_gamma <- sapply(0:30, function(gamma){
   cat("gamma:", gamma, '\n')
   dat_test_3 <- df_prueba %>% 
     mutate(rating_mb = media_gral_train + 
@@ -135,14 +73,16 @@ errores_gamma <- sapply(0:50, function(gamma){
 gamma_min <- errores_gamma %>% 
   filter(error == min(error))
 
-errores_gamma %>% 
+(errores_gamma %>% 
   ggplot(aes(x = gamma, y = error)) + 
   geom_line(aes(group = 1)) + 
   geom_point() +
-  geom_abline(slope = 0, intercept = gamma_min$error, color = 'red') +
-  geom_vline(xintercept = gamma_min$gamma, color = 'red') +
-  theme_minimal() +
-  ggsave(paste0(folder_plots, "/modelo_base.png"))
+  geom_abline(slope = 0, intercept = gamma_min$error, linetype = 'dotted') +
+  geom_vline(xintercept = gamma_min$gamma, linetype = 'dotted') +
+  theme_bw()) %>% 
+  ggsave(., 
+         file = paste0(folder_plots, "/modelo_base.png"),
+         dpi = 500)
 
 write.table(errores_gamma, 
             file = paste0(folder_tables, "/errores_modelo_base.psv"),
