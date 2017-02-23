e3c115ccc658aff0c722b5d0704931e0b8805da1
Dell <mbecerracontreras@gmail.com>
2017-02-22 19:31:31 -0600

    borro archivos que no se usan

--- code/calcula_modelos_finales.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-
-time <- substr(Sys.time(), 1, 19) %>% gsub("[ :]", "_", .)
-
-system(paste('Rscript calcula_modelo_fact_final.R MovieLens', time))
-
-system(paste('Rscript calcula_modelo_fact_final.R BookCrossing', time))
